eaaeb20d86b510f56101bffad6be88ebec06a3f3
veer-chheda <veerchheda3525@gmail.com>
2023-11-13 15:35:20 +0530

    added files for hosting

---
+++ pages/index.tsx
@@ -0,0 +1,48 @@
+import { useAddress } from "@thirdweb-dev/react";
+import styles from "../styles/Home.module.css";
+import { NextPage } from "next";
+import UserStatus from "../components/userStatus";
+import StatusEvents from "../components/statusEvents";
+import { useEffect, useState } from "react";
+import Lottie from "lottie-react";
+import loadingLottie from "../public/loadingLottie.json";
+
+const Home: NextPage = () => {
+  const address = useAddress();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+      const timer = setTimeout(() => {
+          setIsLoading(false);
+      }, 5000);
+
+      return () => clearTimeout(timer);
+  }, []);
+
+  if(isLoading) {
+      return (
+          <div className={styles.pageLoading}>
+              <div>
+                  <Lottie
+                      animationData={loadingLottie}
+                      loop={true}
+                  />
+              </div>
+          </div>
+      );
+  };
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.statusContainer}>
+          <UserStatus />
+        </div>
+        <h3>Status Feed:</h3>
+        <StatusEvents />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
